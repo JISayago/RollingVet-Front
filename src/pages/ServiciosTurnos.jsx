9b594f1188ba5ee5ab940758938114bb317a206a
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2024-09-25 10:17:28 -0300

    cambios menores para poder mostrar las propiedades con el nombre correcto guardado en base de datos

--- src/pages/ServiciosTurnos.jsx
+++ src/pages/ServiciosTurnos.jsx
@@ -1,36 +1,24 @@
-import React from 'react';
+import  { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import clienteAxios from '../helpers/axios.config';
 
 // Array de servicios basado en el NavDropdown
-const servicios = [
-  {
-    id: 1,
-    titulo: 'Consulta Veterinaria',
-    descripcion: 'Consulta general para el cuidado y bienestar de tu mascota.',
-  },
-  {
-    id: 2,
-    titulo: 'Cirugías',
-    descripcion: 'Realizamos cirugías de todo tipo, garantizando el mejor cuidado.',
-  },
-  {
-    id: 3,
-    titulo: 'Peluquería',
-    descripcion: 'Servicio completo de peluquería para mascotas.',
-  },
-  {
-    id: 4,
-    titulo: 'Visitas a Domicilio',
-    descripcion: 'Nos encargamos de cuidar a tu mascota en la comodidad de tu hogar.',
-  },
-];
+
 
 // Componente de Servicios
 const ServiciosCards = () => {
+  const [servicios, setServicios] = useState([]);
+  const obtenerServicios = async () => {
+    const servciosBD = await clienteAxios.get('/servicios')
+    setServicios(servciosBD.data);
+  }
+  useState(() => {
+    obtenerServicios();
+  })
   return (
     <Container className="my-5">
       {/* Título centralizado */}
@@ -42,7 +30,7 @@ const ServiciosCards = () => {
           <Col xs={12} md={6} lg={4} key={servicio.id}>
             <Card className="h-100 text-center"> {/* Centramos el contenido de la card */}
               <Card.Body className="d-flex flex-column justify-content-center align-items-center">
-                <Card.Title>{servicio.titulo}</Card.Title>
+                <Card.Title>{servicio.nombre}</Card.Title>
                 <Card.Text>{servicio.descripcion}</Card.Text>
                 <Button variant="primary">Sacar turno</Button>
               </Card.Body>
