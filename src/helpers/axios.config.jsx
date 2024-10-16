515ebe58cc60007e3fec991ff5b00f7610756521
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2024-10-16 17:20:49 -0300

    cambios url backend

--- src/helpers/axios.config.jsx
+++ src/helpers/axios.config.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL:`${import.meta.env.VITE_URL_BACK_LOCAL}`
+    //baseURL:`${import.meta.env.VITE_URL_BACK_LOCAL}`
+    baseURL: "https://rollingvet-back.onrender.com"
 })
 
 export default clienteAxios;
